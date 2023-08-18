0ce64490333a953a04d7b10a6658da55e6a9412f
aidenfmunro <aidenfmunro@gmail.com>
2023-08-18 14:42:41 +0300

    Update SqrSlvr.cpp
    Added an output and double compare functions. Spaces, brackets for if/else statements.

--- SqrSlvr.cpp
+++ SqrSlvr.cpp
@@ -1,56 +1,129 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <conio.h>
 
-double input();
-int linear_eq(double b, double c);
-int square_eq(double a, double b, double c);
+#define EPS 1e-9
 
-int main(){
-    double a = 0;
-    double b = 0;
-    double c = 0;
+int input(void);
+int output(void);
+int linear_solver(double b, double c);
+int square_solver(double a, double b, double c);
+bool isAlmostEqual(double a, double b);
+
+double coefficients[3] = {0, 0, 0};
+char letters[3] = {'a', 'b', 'c'};
+double roots[2] = {0, 0};
+
+
+int main()
+{
     printf("Please type in the coefficients a, b, c \n");
-    printf("Coefficient a:\n");
-    a = input();
-    printf("Coefficient b:\n");
-    b = input();
-    printf("Coefficient c:\n");
-    c = input();
-    if ((int)a == 0)
-        linear_eq(b, c);
-    else
-        square_eq(a, b, c);
+
+    input();
+
+    output();
 }
 
-int linear_eq(double b, double c){
-    if ((int)b == 0)
-        printf("Invalid expression.");
+
+int linear_solver(double b, double c)
+{
+    if (isAlmostEqual(b, 0))
+      {
+        return 1;
+      }
     else
-        printf("One root: %lf", - c / b);
+      {
+        roots[0] = - c / b;
+      }
+
     return 0;
 
 }
 
-int square_eq(double a, double b, double c){
+
+int square_solver(double a, double b, double c)
+{
     double d, sqrt_d;
     d = pow(b, 2) - 4 * a * c;
     sqrt_d = sqrt(d);
+
     if (d < 0)
-        printf("Oops! Your equation has no real roots.");
-    else if ((int)d == 0)
-        printf("One root: %lf", -b / (2 * a));
+      {
+        return 1;
+      }
+    else if (isAlmostEqual(d, 0))
+      {
+        roots[0] = -b / (2 * a);
+        return 2;
+
+      }
     else
-        printf("Two roots: %lf and %lf", (-b + sqrt_d) / (2 * a), (-b - sqrt_d) / (2 * a));
+      {
+        roots[0] = (-b + sqrt_d) / (2 * a);
+        roots[1] =  (-b - sqrt_d) / (2 * a);
+      }
+
     return 0;
 }
 
-double input(void){
-    double coef;
-    if (scanf("%lf", &coef) != 0)
-        return coef;
-    else
-        printf("Incorrect input, try again.");
-        exit(0);
-    return coef;
+
+int input(void)
+{
+    for (int i = 0; i <= 2; ++i)
+        {
+
+        int res = 0;
+
+        do {
+            printf("Type in coefficient %c\n", letters[i]);
+            res = scanf("%lf", &coefficients[i]);
+            fflush(stdin);
+           } while (res != 1);
+
+        }
+
+    return 0;
 }
+
+
+int output(void)
+{
+    if (isAlmostEqual(coefficients[0], 0))
+        {
+            if (linear_solver(coefficients[1], coefficients[2]) == 1)
+            {
+                printf("Invalid expression");
+            }
+            else
+            {
+                printf("One root: %lf", roots[0]);
+            }
+        }
+    else
+        {
+            if (square_solver(coefficients[0], coefficients[1], coefficients[2]) == 1)
+            {
+                printf("Oops! Your equation has no real roots.");
+            }
+            else if (square_solver(coefficients[0], coefficients[1], coefficients[2]) == 2)
+            {
+                printf("One root: %lf", roots[0]);
+            }
+            else
+                printf("Two roots: %lf and %lf", roots[0], roots[1]);
+        }
+
+    return 0;
+}
+
+
+
+bool isAlmostEqual(double a, double b)
+{
+    return fabs(a - b) < EPS;
+}
+
+
+
