5439f42973864a96f381ecd2d22249f3ec3625de
aidenfmunro <aidenfmunro@gmail.com>
2023-08-23 18:49:00 +0300

    Update
    Added command line call

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <TXLib.h>
+#include <string.h>
 #include "../headers/solvingutil.h"
 #include "../headers/ioutil.h"
 #include "../headers/tester.h"
 
-int main()
+int main(int n, char *argv[])
 {
-    double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
+    if (strcmp (argv[1], "t-") != 0)
+      {
+        double a = NAN, b = NAN, c = NAN, x1 = NAN, x2 = NAN;
 
-    input(&a, &b, &c);
+        input(&a, &b, &c);
 
-    int numRoots = equationSolver(a, b, c, &x1, &x2);
+        int numRoots = equationSolver(a, b, c, &x1, &x2);
 
-    output(numRoots, &x1, &x2);
-
-    // testEquation();
+        output(numRoots, &x1, &x2);
+      }
+    else if (strcmp (argv[1], "solve") != 0)
+      {
+        testEquation();
+      }
 }
